ca9bf8045c92278880cdd732fd0d88a2fd088c89
Mariusz H <zarki1@wp.pl>
2017-01-19 19:50:49 +0100

    Dodawanie pakietów nuget + bower

--- ServiceMap/src/ServiceMap/Startup.cs
+++ ServiceMap/src/ServiceMap/Startup.cs
@@ -16,6 +16,7 @@ namespace ServiceMap
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +28,10 @@ namespace ServiceMap
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseBrowserLink();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
