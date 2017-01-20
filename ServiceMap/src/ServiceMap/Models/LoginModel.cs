abd66f502875b25aec0b333a2e1328a4fdf7b08e
Mariusz H <zarki1@wp.pl>
2017-01-20 01:38:37 +0100

    Dalsze konfiguracja aplikacji

---
+++ ServiceMap/src/ServiceMap/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceMap.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Login { get; set; }
+        [UIHint("password")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
--- ServiceMap/src/ServiceMap/Startup.cs
+++ ServiceMap/src/ServiceMap/Startup.cs
@@ -7,36 +7,78 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace ServiceMap
 {
     public class Startup
     {
+        IConfiguration Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // TODO str 214
+            //services.AddDbContext<Applica>
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            // Odwłuje się do appsettings.json -> Logging
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
 
             app.UseBrowserLink();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
+
             app.UseMvcWithDefaultRoute();
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                name: "Error", 
+                template: "Error",
+                defaults : new { controller = "Error", action = "Error" });
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
             });
+            //app.UseIdentity();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
