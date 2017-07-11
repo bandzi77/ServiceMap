9eb693f4c140fadd82b51d041c9548fa38e23323
Mariusz H <zarki1@wp.pl>
2017-07-11 22:01:53 +0200

    Revert "Literówka"
    This reverts commit 64629faacb6e8a1b4ef7b8d591ce2d215f75e887.

--- ServiceMap/src/ServiceMap/wwwroot/app/app.module.js
+++ ServiceMap/src/ServiceMap/wwwroot/app/app.module.js
@@ -27,7 +27,7 @@ AppModule = __decorate([
         imports: [
             platform_browser_1.BrowserModule,
             http_1.HttpModule,
-            // TODO - Do usuniecia
+            // TODO - Do usunięcia
             forms_1.ReactiveFormsModule,
             router_1.RouterModule.forRoot([
                 //{ path: 'welcome', component: WelcomeComponent },
