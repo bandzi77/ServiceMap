13f27634560aa44117820a04348ffb280b9beaf9
Mariusz H <zarki1@wp.pl>
2017-07-11 22:02:35 +0200

    Lietrówka w js

--- ServiceMap/src/ServiceMap/wwwroot/app/app.module.js
+++ ServiceMap/src/ServiceMap/wwwroot/app/app.module.js
@@ -27,7 +27,7 @@ AppModule = __decorate([
         imports: [
             platform_browser_1.BrowserModule,
             http_1.HttpModule,
-            // TODO - Do usunięcia
+            // TODO - Do usuniecia
             forms_1.ReactiveFormsModule,
             router_1.RouterModule.forRoot([
                 //{ path: 'welcome', component: WelcomeComponent },
