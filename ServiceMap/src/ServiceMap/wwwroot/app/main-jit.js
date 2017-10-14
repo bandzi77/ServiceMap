96c1e64508df02b704551e1019d27293c314e6db
Mariusz H <zarki1@wp.pl>
2017-10-14 17:48:57 +0200

    Wersja po zmianie klauzuli na pierwszej stronie.
    Zmiany w gitignore

--- ServiceMap/src/ServiceMap/wwwroot/app/main-jit.js
+++ ServiceMap/src/ServiceMap/wwwroot/app/main-jit.js
@@ -9,4 +9,4 @@ if (!/localhost/.test(document.location.host)) {
     core_1.enableProdMode();
 }
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main-jit.js.map
